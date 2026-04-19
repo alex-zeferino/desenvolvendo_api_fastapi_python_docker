39932bbfaf73b9e19c2d11a208b72293f719c787
Alexsander Zeferino <63205622+alex-zeferino@users.noreply.github.com>
2026-04-19 15:19:59 -0300

    Delete workout_api directory

--- workout_api/configs/settings.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-from pydantic import Field
-
-class Settings(BaseSettings):
-    DB_URL:str = Field(default=driver://user:pass@localhost/dbname)
-    
-    
-settings = Settings()
